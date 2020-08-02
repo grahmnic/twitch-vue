42bf9d4916dee3730a0e4edebfd83123ab18477a
Nicholas Chen <grahmnic@gmail.com>
2020-08-01 22:04:14 -0400

    finished vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,159 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="root">
+    <div class="wrapper">
+      <input type="text" v-model="username" spellcheck="false" placeholder="Enter channel name" />
+      <Button :callback="getChannelInfo" width="6rem" height="3rem">
+        <i class="fa fa-search"></i>
+      </Button>
+    </div>
+    <div v-if="hasLoaded" class="result">
+      <span>{{loadResult}}</span>
+    </div>
+    <div v-if="loading" class="loader">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// BASE COMPONENTS
+import Button from './components/Button';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Button
+  },
+  data: function() {
+    return {
+      username: "",
+      foo: {
+        followers: "",
+        channel: ""
+      },
+      dataLoaded: false,
+      loading: false
+    }
+  },
+  computed: {
+    hasLoaded: function() {
+      return this.dataLoaded && !this.loading;
+    },
+    loadResult: function() {
+      return this.dataLoaded && !this.loading && this.foo.error ? 
+        this.foo.error : 
+        this.foo.channel + " has " + this.foo.followers + " followers.";
+    }
+  },
+  methods: {
+    getChannelInfo() {
+      this.loading = true;
+
+      const requestOptions = {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'}
+      };
+      let baseUrl = 'twitch-vue.herokuapp.com' === window.location.origin ? 'twitch-vue.herokuapp.com' : 'http://localhost:3000/';
+      fetch(`${baseUrl}channels/${this.username}`, requestOptions)
+        .then(response => {
+          if (response.ok)
+            return response.json();
+        })
+        .then(response_data => {
+          this.loading = false;
+          this.dataLoaded = true;
+          this.foo = response_data;
+        })
+        .catch(err => console.log(err));
+    },
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap');
+body {
+  margin: 0;
+}
+#root {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  background: url('./assets/dark-geometric.png');
+  background-color: $dark;
+  font-family: 'Roboto Condensed', sans-serif;
+}
+.wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 30px;
+  div {
+    position: relative;
+  }
+}
+input {
+  font-size: 1.5rem;
+  font-family: inherit;
+  margin-right: 50px;
+  color: $primary-light;
+  caret-color: $primary;
+  background: transparent;
+  border: none;
+  border-bottom: 3px solid $primary-light;
+  padding: 5px 8px;
+  &:focus {
+    outline: none;
+  }
+  &::placeholder {
+    color: $primary;
+  }
+  &::selection {
+    background-color: $primary-light;
+  }
+}
+@keyframes fadeInLeft {
+  from { opacity: 0; transform: translateX(-25%)}
+  to { opacity: 1; transform: translateX(0)}
+}
+.result {
+  color: $primary-light;
+  font-size: 2rem;
+  word-spacing: 5px;
+  animation: fadeInLeft 0.5s;
+  span {
+    font-size: 3rem;
+      font-weight: bold;
+  }
+}
+$big: 0.75rem;
+$small: 0.4rem;
+@keyframes lateLoading {
+  0% { opacity: 0.5; width: $small; height: $small}
+  50% { opacity: 0.7; width: $big; height: $big;}
+  100% { opacity: 0.5; width: $small; height: $small;}
+}
+@keyframes loading {
+  0% { opacity: 0.7; width: $big; height: $big;}
+  50% { opacity: 0.5; width: $small; height: $small;}
+  100% { opacity: 0.7; width: $big; height: $big;}
+}
+.loader {
+  height: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  div{
+    border-radius: 50%;
+    margin: 0px 4px;
+    background-color: $primary-light;
+    animation: lateLoading 0.25s infinite;
+    &:nth-child(2n) {
+      animation: loading 0.25s infinite;
+    }
+  }
 }
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,53 @@
+<template>
+  <button v-on:click="callback" :style="style">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    callback: Function,
+    width: String,
+    height: String
+  },
+  computed: {
+    style() {
+      return 'width: ' + this.width + 
+             ';height: ' + this.height + ';';
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+  border: none;
+  font-size: 1.5rem;
+  border-radius: 0.3rem;
+  padding: 4px 6px;
+  color: $light;
+  background-color: $primary;
+  width: 4rem;
+  height: 1.5rem;
+  cursor: pointer;
+  border: 1px solid $primary;
+  transition: all 0.25s ease-in-out;
+  transform: translateY(-2px);
+  box-shadow: 0px 2px $primary-dark, 0px 3px $primary-dark;
+  &:hover {
+    transform: translateY(-5px);
+    box-shadow: 0px 5px $primary-dark, 0px 6px $primary-dark;
+  }
+  &:active {
+    transform: translateY(1px);
+    background-color: $primary-dark;
+    color: $off-white;
+    box-shadow: none;
+  }
+  &:focus {
+    outline: none;
+  }
+}
+</style>
